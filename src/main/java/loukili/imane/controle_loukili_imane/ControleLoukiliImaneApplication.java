c3105cb7a47d32286fb439115eee233c3bfd740c
Imane-Loukili <i.loukili@etu.enset-media.ac.ma>
2023-04-17 08:43:04 +0000

    first commit

---
+++ src/main/java/loukili/imane/controle_loukili_imane/ControleLoukiliImaneApplication.java
@@ -0,0 +1,13 @@
+package loukili.imane.controle_loukili_imane;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleLoukiliImaneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ControleLoukiliImaneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/loukili/imane/controle_loukili_imane/ControleLoukiliImaneApplicationTests.java
@@ -0,0 +1,13 @@
+package loukili.imane.controle_loukili_imane;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleLoukiliImaneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
